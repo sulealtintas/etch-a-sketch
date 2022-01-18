0f4d027589d5cda4483793183e6fbec37161c7c0
sua <sua@netcompany.com>
2022-01-18 20:53:43 +0100

    add rainbow mode

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const grid = document.querySelector("#grid");
+let gridSize = 16;
+let drawingMode = "classic";
+
+function resetGrid() {
+    grid.innerHTML = "";
+    createGrid(gridSize, drawingMode);
+}
+
+function setDrawingMode(mode) {
+    if (drawingMode !== mode) {
+        drawingMode = mode;
+        resetGrid();
+    }
+}
+
+function drawClassic() {
+    this.style.backgroundColor = "black";
+}
+
+function drawRainbow() {
+    console.log(this.style.backgroundColor);
+    hue = Math.floor(Math.random() * 361).toString();
+    this.style.backgroundColor = `hsl(${hue},100%,70%)`;
+}
 
 function createGrid() {
-    for (let i = 0; i < 16 ** 2; i++) {
+    for (let i = 0; i < gridSize ** 2; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
-        div.addEventListener("mouseover", function () {
-            this.style.backgroundColor = "black";
-        })
+
+        if (drawingMode === "classic") {
+            div.addEventListener("mouseover", drawClassic);
+        } else if (drawingMode === "rainbow") {
+            div.addEventListener("mouseover", drawRainbow);
+        }
+
         grid.appendChild(div);
     }
 }
